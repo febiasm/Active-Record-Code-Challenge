d9e31ac656231180d2d66ae12080bbeee0254b39
febiasm <trevorfebias@gmail.com>
2023-02-28 00:01:37 +0300

    edited comments

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,14 @@
 class Review < ActiveRecord::Base
+  # Associations
   belongs_to :user
   belongs_to :product
 
+  # Validations
   validates :star_rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :comment, presence: true, length: { maximum: 1000 }
   validates :star_rating, numericality: { only_integer: true, message: "rating must be a whole number" }
 
+  # reviews
   def print_review
     puts "The following review was written by #{self.user.name} for the product #{self.product.name}."
     puts "It received a star rating of #{self.star_rating} and the comment was: #{self.comment}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
+  # A user can have many reviews
   has_many :reviews
+  # A user can have many products through their reviews
   has_many :products, through: :reviews
 
+  # Returns the user's favorite product based on the highest rated review they've left
   def favorite_product
+      # Orders the user's reviews by star rating in descending order (highest first)
+      # Returns the first review (highest rated) from the ordered list
       products.order("reviews.star_rating DESC").first
   end
 
+  # Removes all reviews by the user for a given product
   def remove_reviews(product)
+      # Find all reviews by the user for the given product
       reviews.where(product_id: product.id).destroy_all
   end
-
 end
