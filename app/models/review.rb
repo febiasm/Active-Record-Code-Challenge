12b1e0507048945f3d05b6a6b43854c2d672489c
febiasm <trevorfebias@gmail.com>
2023-02-28 10:50:57 +0300

    fixed reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ActiveRecord::Base
 
   # Validations
   validates :star_rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :comment, presence: true, length: { maximum: 1000 }
+  validates :comment, presence: true, length: { maximum: 100 }
   validates :star_rating, numericality: { only_integer: true, message: "rating must be a whole number" }
 
   # reviews
